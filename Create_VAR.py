9a4412af7209ff172e20fbbba1e6d8e8c9344bed
sunilnaths <30995634+sunilnaths@users.noreply.github.com>
2021-04-12 23:05:43 -0500

    Update and rename ANS_NOR_VAR.py to Create_VAR.py

--- Create_VAR.py
+++ Create_VAR.py
@@ -77,25 +77,3 @@ print(final_list)
 
 with open(r'Var-Nornir.yaml', 'w') as file:
     documents = yaml.dump(final_list, file, default_flow_style=False)
-
-
-def load_vars(task1):
-    data = task1.run(task=load_yaml, file='Var-Nornir.yaml')
-    task1.host["facts"] = data.result
-    print_result(data.result)
-    basic_configuration(task1)
-
-
-def basic_configuration(task1):
-    r = task1.run(task=template_file, template="L2_VxLAN1.j2",
-                  path='')
-    task1.host["Var-Nornir.yaml"] = r.result
-    vlan_output = task1.host["Var_Nornir.yaml"]
-    vlan_send = vlan_output.splitlines()
-    task1.run(task=netmiko_send_config, name="VLAN Commands",
-              config_commands=vlan_send)
-
-
-print_title("config")
-result = nr.run(task=load_vars)
-print_result(result)
